a001c1ca433e281175639165f003f1c5eb27ca4c
Omer GOKSOY <goksoy.omer@gmail.com>
2023-01-07 02:25:19 +0300

    blok durum kitaplığında düzeltme

--- Cache/Transaction.cs
+++ Cache/Transaction.cs
@@ -9,95 +9,37 @@ using System.Text.Json;
 using System.Threading.Tasks;
 using NVE = Notus.Variable.Enum;
 using NVG = Notus.Variable.Globals;
+using NVS = Notus.Variable.Struct;
 namespace Notus.Cache
 {
     public class Transaction : IDisposable
     {
-        //transaction'ın listeye eklenme zamanı
-        private SortedDictionary<ulong, string> txCheck = new SortedDictionary<ulong, string>();
-        private Dictionary<string, ulong> txTime = new Dictionary<string, ulong>();
-
-        // transaction'ın durumunu tutan değişken
-        private Dictionary<string, NVE.BlockStatusCode> txStatus = new Dictionary<string, NVE.BlockStatusCode>();
-
-        // transaction'ın hangi blokta olduğunu tutan liste
-        //private Dictionary<string, ulong> txList = new Dictionary<string, ulong>();
-
-        private Notus.Threads.Timer TimerObj;
-        private Notus.Mempool MP_BlockPoolList;
+        private Notus.Data.KeyValue keyValue = new();
         public NVE.BlockStatusCode Status(string blockUid)
         {
+            string rawDataStr = keyValue.Get(blockUid);
+            try
+            {
+                NVE.BlockStatusCode tmpstatus = JsonSerializer.Deserialize<NVE.BlockStatusCode>(rawDataStr);
+                return tmpstatus;
+            }
+            catch { }
             return NVE.BlockStatusCode.Unknown;
         }
         public void Add(string blockUid, NVE.BlockStatusCode statusCode)
         {
-            if (txTime.ContainsKey(blockUid))
-            {
-                // eski zamana ait kayıt siliniyor...
-                txCheck.Remove(txTime[blockUid]);
-
-                // yeni kontrol kaydı ekleniyo
-                ulong tmpRightNow = NVG.NOW.Int;
-                txCheck.Add(tmpRightNow, blockUid);
-
-                // diğerleri güncelleniyor
-                txTime[blockUid] = tmpRightNow;
-                txStatus[blockUid] = statusCode;
-            }
-            else
-            {
-                ulong tmpRightNow = NVG.NOW.Int;
-                bool exitInnerLoop = false;
-                while (exitInnerLoop == false)
-                {
-                    if (txCheck.ContainsKey(tmpRightNow) == true)
-                    {
-                        if(string.Equals(txCheck[tmpRightNow], blockUid) == false)
-                        {
-                            tmpRightNow = tmpRightNow + 1;
-                        }
-                        else
-                        {
-                            exitInnerLoop = true;
-                        }
-                    }
-                    else
-                    {
-                        exitInnerLoop = true;
-                    }
-                }
-                txCheck.Add(tmpRightNow, blockUid);
-                txTime.Add(blockUid, tmpRightNow);
-                txStatus.Add(blockUid, statusCode);
-            }
-        }
-        public void Set2(string blockUid, NVE.BlockStatusCode statusCode)
-        {
-            string bgInt = BigInteger.Parse("0" + blockUid, NumberStyles.AllowHexSpecifier).ToString();
-            string lastHexStr = bgInt.Substring(bgInt.Length - 2);
-
-            string dbName =
-                Notus.IO.GetFolderName(NVG.Settings, Notus.Variable.Constant.StorageFolderName.TxList) +
-                Notus.Variable.Constant.MemoryPoolName["TransactionList"] + lastHexStr + ".db";
-            using (MP_BlockPoolList = new Notus.Mempool(dbName))
-            {
-                MP_BlockPoolList.AsyncActive = false;
-            }
-        }
-        public void Set(string blockUid, NVE.BlockStatusCode statusCode)
-        {
-            txStatus.Add(blockUid, statusCode);
+            keyValue.Set(blockUid, JsonSerializer.Serialize(statusCode));
         }
         public Transaction()
         {
-            TimerObj = new Notus.Threads.Timer(100);
-            TimerObj.Start(() =>
+            keyValue.SetSettings(new NVS.KeyValueSettings()
             {
-                if (txStatus.Count > 0)
-                {
-
-                }
-            }, true);
+                LoadFromBeginning = true,
+                ResetTable = false,
+                Path = "block_meta",
+                MemoryLimitCount = 1000,
+                Name = "block_status"
+            });
         }
         ~Transaction()
         {
@@ -107,14 +49,9 @@ namespace Notus.Cache
         {
             try
             {
-                if (TimerObj != null)
-                {
-                    TimerObj.Dispose();
-                }
+                keyValue.Dispose();
             }
-            catch (Exception err)
-            {
-            }
+            catch { }
         }
     }
 }
