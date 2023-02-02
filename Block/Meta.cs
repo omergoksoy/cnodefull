162164b5b53493f102b9015c21c8629a36fccf77
Omer GOKSOY <goksoy.omer@gmail.com>
2023-02-03 01:33:51 +0300

    hata metni yazıldı

--- Block/Meta.cs
+++ Block/Meta.cs
@@ -318,6 +318,7 @@ namespace Notus.Block
 
             // every time "NVC.NodeValidationModCount" mod is Zero
             string tmpstateKey = GetStateKey(chainId, currentState.rowNo);
+            Console.WriteLine("tmpstateKey : " + tmpstateKey);
             stateDb.Set(tmpstateKey, allSignStr);
 
             // control_noktasi();
