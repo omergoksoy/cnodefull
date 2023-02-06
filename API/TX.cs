e113493d2d15d48298e3f758fefa9805b6071c22
Omer GOKSOY <goksoy.omer@gmail.com>
2023-02-06 23:50:35 +0300

    API noktaları test ediliyor

--- API/TX.cs
+++ API/TX.cs
@@ -12,20 +12,24 @@ namespace Notus.API
         public static string Process(NVS.HttpRequestDetails IncomeData, bool ToDistribute)
         {
             if (IncomeData.UrlList.Length < 2)
-                return JsonSerializer.Serialize(false);
+                //return JsonSerializer.Serialize(false);
+                return "cikis-1";
 
             //string tmpText = "f84583312e308b616c6963695f61647265738574757461728c69736c656d5f7563726574698c6e6f6e63655f646567657269846461746184696d7a618a7075626c69635f6b6579";
             string tmpText = IncomeData.UrlList[1];
             IList<string> result = NE.RLP.Decode(tmpText);
 
             if (result.Count != 8)
-                return JsonSerializer.Serialize(false);
+                return "cikis-2";
+            //return JsonSerializer.Serialize(false);
 
             if (string.Equals(result[0], "1.0") != false)
-                return JsonSerializer.Serialize(false);
+                return "cikis-3";
+            //return JsonSerializer.Serialize(false);
 
             if (result[1].Length != NVC.WalletFullTextLength)
-                return JsonSerializer.Serialize(false);
+                return "cikis-4";
+            //return JsonSerializer.Serialize(false);
 
             List<string> dataList = new List<string> {
                     "1.0",          // version
@@ -45,12 +49,13 @@ namespace Notus.API
                 if (i < (rawDataCount - 1))
                     rawDataText += ":";
             }
+            return JsonSerializer.Serialize(result);
+
             bool isIverified = Notus.Wallet.ID.Verify(rawDataText, dataList[6], dataList[7]);
             if (isIverified == false)
             {
                 return JsonSerializer.Serialize(false);
             }
-            return JsonSerializer.Serialize(result);
         }
     }
 }
