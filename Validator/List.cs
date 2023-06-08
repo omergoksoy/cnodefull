d119bd862797c750fb621ac98d0355b4eb405b56
Omer GOKSOY <42944329+omergoksoy@users.noreply.github.com>
2023-06-08 19:45:54 +0300

    Update List.cs

--- Validator/List.cs
+++ Validator/List.cs
@@ -17,11 +17,6 @@ namespace Notus.Validator
                         Variable.Enum.NetworkType.MainNet,
                         new List<Variable.Struct.IpInfo>()
                         {
-                            //test node
-                            //{ new Variable.Struct.IpInfo() { IpAddress = "18.156.37.61", Port = 5000 } },
-                            //{ new Variable.Struct.IpInfo() { IpAddress = "3.125.159.102", Port = 5000 } }
-                            
-                            // real node ip
                             { new Variable.Struct.IpInfo() { IpAddress = "3.68.233.67", Port = 5000 } },
                             { new Variable.Struct.IpInfo() { IpAddress = "3.75.243.44", Port = 5000 } }
                         }
@@ -30,11 +25,6 @@ namespace Notus.Validator
                         Variable.Enum.NetworkType.TestNet,
                         new List<Variable.Struct.IpInfo>()
                         {
-                            //test node
-                            //{ new Variable.Struct.IpInfo() { IpAddress = "18.156.37.61", Port = 5001 } },
-                            //{ new Variable.Struct.IpInfo() { IpAddress = "3.125.159.102", Port = 5001 } }
-                            
-                            // real node ip
                             { new Variable.Struct.IpInfo() { IpAddress = "3.68.233.67", Port = 5001 } },
                             { new Variable.Struct.IpInfo() { IpAddress = "3.75.243.44", Port = 5001 } }
                         }
@@ -43,13 +33,8 @@ namespace Notus.Validator
                         Variable.Enum.NetworkType.DevNet,
                         new List<Variable.Struct.IpInfo>()
                         {
-                            //test node
-                            //{ new Variable.Struct.IpInfo() { IpAddress = "18.156.37.61", Port = 5002 } },
-                            //{ new Variable.Struct.IpInfo() { IpAddress = "3.125.159.102", Port = 5002 } }
-                            
-                            // real node ip
-                            { new Variable.Struct.IpInfo() { IpAddress = "3.68.233.67", Port = 5002 } },
-                            { new Variable.Struct.IpInfo() { IpAddress = "3.75.243.44", Port = 5002 } }
+                            { new Variable.Struct.IpInfo() { IpAddress = "18.156.37.61", Port = 5002 } },
+                            { new Variable.Struct.IpInfo() { IpAddress = "3.125.159.102", Port = 5002 } }
                         }
                     }
                 }
